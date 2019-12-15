af1b14430500c5e453500374b55efa0df383124b
korea <643949593@qq.com>
2019-12-15 19:30:30 +0800

    react-swiper

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, {Component} from "react";
 import Banner from "./components/Banner";
 
-let IMG_DATA = [];
-for (let i = 1; i<= 3; i++) {
-    IMG_DATA.push({
-        id: i,
-        src: require(`./static/image/img${i}.webp`),
-        title: `图片${i}张`
-    })
-}
-
 class App extends  Component{
     render() {
         return (
             <div>
-                <Banner data={IMG_DATA} step={1} speed={300} interval={1000}/>
+                <Banner />
             </div>
         )
     }
